088260fd3cb99e4e4ce6794041fd1908d708112c
seelananton8-byte <seelananton8@gmail.com>
2025-12-30 13:08:11 +0530

    Add a Error Handlers in Fetching...

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,63 @@ const apiKey = import.meta.env.VITE_API_KEY;
 function App() {
   const [movies, setMovies] = useState([])
   const [bannerMovie, setBannerMovie] = useState(null)
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchMovies()
   }, [])
 
   async function fetchMovies() {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`
-    )
-    const data = await response.json()
-    setMovies(data.results)
-    setBannerMovie(data.results[0])
+    try {
+      setLoading(true);
+      setError(null);
+
+      const res = await fetch(
+        `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`
+      );
+
+      if (!res.ok) {
+        throw new Error(`API Error: ${res.status}`);
+      }
+
+      const data = await res.json();
+
+      if (!data.results?.length) {
+        throw new Error("No data available");
+      }
+
+      setMovies(data.results);
+      setBannerMovie(data.results[0]);
+
+    } catch (err) {
+      console.error(err);
+      setError(err.message);
+      setBannerMovie(null);
+    } finally {
+      setLoading(false);
+    }
   }
 
-  return(
+  return (
     <Fragment>
       <Navbar />
-      {bannerMovie && <Banner 
-      movie={bannerMovie}  />}
-      <p style={{color: 'yellowgreen', marginLeft: '20px', paddingTop: '30px'}}>ONLINE STREAMING</p>
-      <Button />
-      <MovieRow title="Upcoming Movies" movies={movies} />
+
+      {loading && <p style={{ color: "white" }}>Loading...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {!error && bannerMovie && <Banner movie={bannerMovie} />}
+
+      {!error && (
+        <>
+          <p style={{ color: "yellowgreen", marginLeft: "20px", paddingTop: "30px" }}>
+            ONLINE STREAMING
+          </p>
+          <Button />
+          <MovieRow title="Upcoming Movies" movies={movies} />
+        </>
+      )}
     </Fragment>
-  )
+  );
 }
 export default App
